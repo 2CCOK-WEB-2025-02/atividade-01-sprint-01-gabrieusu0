ae2dfea045664c2053e0c023b06b5a76780c139b
gabrieusu0 <gabriel.osantos@sptech.school>
2025-08-17 16:43:23 -0300

    Adicionando resoluções

--- src/main/java/school/sptech/sprint1_nota1/ex1/ExercicioFacilController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex1/ExercicioFacilController.java
@@ -1,13 +1,21 @@
 package school.sptech.sprint1_nota1.ex1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExercicioFacilController {
 
     @GetMapping("/ex-01/{palavra}")
-    public Boolean exercicioFacil(String palavra) {
-        return null;
+    public Boolean exercicioFacil(@PathVariable String palavra) {
+        int tamanho = palavra.length();
+
+        for (int i = 0; i < tamanho / 2; i++) {
+            if (palavra.toLowerCase().charAt(i) != palavra.toLowerCase().charAt(tamanho - i - 1)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
@@ -4,11 +4,44 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class ExercicioDificilController {
 
     @GetMapping("/ex-03/{n}")
-    public ExercicioDificilResponse exercicioDificil(int n) {
-        return null;
+    public ExercicioDificilResponse exercicioDificil(@PathVariable int n) {
+        ExercicioDificilResponse resposta = new ExercicioDificilResponse();
+
+        if (n <= 0){
+            resposta.setEnesimoTermo(0);
+            resposta.setSoma(0);
+            return resposta;
+        }
+
+        int anterior = 0;
+        int atual = 1;
+        int soma = anterior + atual;
+
+        if (n == 1) {
+            resposta.setEnesimoTermo(1);
+            resposta.setSoma(1);
+            return resposta;
+        }
+
+
+        for (int i = 2; i <= n; i++) {
+            int proximo = anterior + atual;
+            anterior = atual;
+            atual = proximo;
+            soma += atual;
+        }
+
+        resposta.setEnesimoTermo(atual);
+        resposta.setSoma(soma);
+
+        return resposta;
     }
 }
+
